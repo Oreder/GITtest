015896775b6a6ca940dc48454c8a374429701a60
Binh D. Nguyen <stepup.ored@gmail.com>
2018-03-21 01:24:31 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	if (argc < 1)
+	{
+		printf("Error #1: Not enough parameters.\n");
+		return -1;
+	}
+	
+	int n = atoi(argv[1]);
+	double s = 0.;
+	for (int j = 1; j <= n; j++)
+		s += j;
+	
+	printf("Sum of all integers in range 1..%d is: %d.\n", n, s);
+	return 0;
+}
