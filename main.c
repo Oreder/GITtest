49c54ed455442d65d5283e535a256b4dc688015b
Binh D. Nguyen <stepup.ored@gmail.com>
2018-03-21 09:48:34 +0300

    change n into (n+1)

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 	
 	int n = atoi(argv[1]);
 	double s = 0.;
-	for (int j = 1; j <= n; j++)
+	for (int j = 1; j < n+1; j++)
 		s += j;
 	
 	printf("Sum of all integers in range 1..%d is: %d.\n", n, s);
